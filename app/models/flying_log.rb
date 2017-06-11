4019f85ee7829fa5ef89aae9360186e5f046a54b
Junaid Atique <abdulbasit558@gmail.com>
2017-06-11 12:54:37 +0500

    sortie and after flight

--- app/models/flying_log.rb
+++ app/models/flying_log.rb
@@ -11,11 +11,14 @@ class FlyingLog
   has_one :ac_configuration
   has_one :fuel
   has_one :capt_acceptance_certificate
+  has_one :sortie
+  has_one :capt_after_flight
   has_many :flightline_servicings
 
   accepts_nested_attributes_for :ac_configuration
   accepts_nested_attributes_for :fuel
-  accepts_nested_attributes_for :flightline_servicings, :reject_if => :all_blank, :allow_destroy => true
   accepts_nested_attributes_for :capt_acceptance_certificate
-
+  accepts_nested_attributes_for :sortie
+  accepts_nested_attributes_for :capt_after_flight
+  accepts_nested_attributes_for :flightline_servicings, :reject_if => :all_blank, :allow_destroy => true
 end
--- app/models/sortie.rb
+++ app/models/sortie.rb
@@ -1,10 +1,18 @@
 class Sortie
   include Mongoid::Document
-
+  include SimpleEnum::Mongoid
+  
+  as_enum :sortie_code, C1: 1, C2: 2, C3: 3, C4: 4, C5: 5
   field :takeoff_time, type: Time
   field :landing_time, type: Time
   field :flight_time, type: String
   field :flight_minutes, type: String
+  field :touch_go, type: String
+  field :full_stop, type: String
+  field :total, type: String
+  field :remarks, type: String
+
 
   belongs_to :user
+  belongs_to :flying_log
 end
